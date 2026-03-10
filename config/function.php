c1a80ca1208294d30c1f1704d41402dae9aed9e6
Dew <158989279+JakrayuSaithong@users.noreply.github.com>
2026-03-10 10:04:02 +0700

    Remove decryptIt function from function.php

--- config/function.php
+++ config/function.php
@@ -632,12 +632,4 @@ function get_userAdmin($admin_status){
 	return $data;
 }
 
-function decryptIt($q)
-{
-	$cryptKey  = 'Iloveyouallpann';
-	$qDecoded = rtrim(openssl_decrypt(base64_decode($q), "AES-256-CBC", md5($cryptKey), 0, substr(md5(md5($cryptKey)), 0, 16)), "\0");
-	//write_file("temp.txt","=".date("H:i:s")."=qDecoded===$qDecoded=\r\n","a");
-	return ($qDecoded);
-}
-
 ?>
